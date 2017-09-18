f66ad3321cb18073516c3d88bbf1668708e6f26e
Christian Nowitzki <nowit001@cougars.csusm.edu>
2017-09-17 21:31:57 -0900

    inital commit
    all the files in my project folder

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour{
+
+    public float speed;
+    private Vector3 position;
+
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    private void Start()
+    {
+        position = new Vector3(0.04f, 0.5f, -2.81f);
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+
+        if (other.gameObject.CompareTag("Death"))
+        {
+            gameObject.transform.position = position;
+            rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+        if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            position = other.gameObject.transform.position + new Vector3(0.0f, 0.5f, 0.0f);
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count >= 10)
+        {
+            winText.text = "YOU WIN!!";
+        }
+    }
+}
---
+++ Assets/scripts/RightAndLeft.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightAndLeft : MonoBehaviour {
+
+    public Vector3 direction;
+    public GameObject platform;
+    public float right;
+    public float left;
+
+    void Start()
+    {
+        direction = new Vector3(-0.03f, 0f, 0f);
+        right = 3f;
+        left = -3f;
+    }
+
+    void Update()
+    {
+        float x_coordinate = platform.transform.position.x;
+        if ((x_coordinate > right) | (x_coordinate < left))
+        {
+            direction = direction * (-1);
+        }
+        platform.transform.position = platform.transform.position + direction;
+
+    }
+}
